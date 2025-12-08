62607be7b693c4524eb9f28ebfaee9f6f837f9a2
Ferdi <hello@ferdi.digital>
2025-12-08 19:39:49 +0100

    feat: implement app layout with navigation and overlay transitions

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,77 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="app">
+    <page-header />
+    <transition name="overlay">
+      <!-- eslint-disable-next-line vue/html-self-closing -->
+      <div v-if="navOpen" class="overlay" @click="navOpen = false"></div>
+    </transition>
+    <transition name="nav">
+      <div v-if="navOpen" class="nav-wrapper" :aria-hidden="!navOpen">
+        <main-navigation />
+      </div>
+    </transition>
+    <nuxt-route-announcer />
+    <main>
+      <div class="max-w">
+        <nuxt-page />
+      </div>
+    </main>
   </div>
 </template>
+
+<script setup>
+const navOpen = useState('nav')
+</script>
+
+<style>
+.app {
+  display: flex;
+  flex-direction: column;
+}
+
+main {
+  flex: 1;
+  overflow-y: auto;
+}
+
+.overlay {
+  background-color: var(--clr-blue-900);
+
+  opacity: .75;
+
+  position: fixed;
+  inset: 0;
+  z-index: 2;
+}
+
+.nav-wrapper {
+  position: fixed;
+  inset: 0;
+  margin-left: auto;
+  z-index: 3;
+
+  @media screen and (min-width: 1260px) {
+    width: 50%;
+  }
+}
+
+.nav-enter-active,
+.nav-leave-active {
+  transition: transform 200ms ease-out;
+}
+
+.nav-enter-from,
+.nav-leave-to {
+  transform: translateX(100%);
+}
+
+.overlay-enter-active,
+.overlay-leave-active {
+  transition: opacity 200ms;
+}
+
+.overlay-enter-from,
+.overlay-leave-to {
+  opacity: 0;
+}
+</style>
