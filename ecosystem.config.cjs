7f4ba7d57748c1e252aa87fc6b02b0c897e5fe21
Ferdi <hello@ferdi.digital>
2025-12-08 20:14:38 +0100

    fix: remove port configuration and ensure proper environment settings

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,13 @@ module.exports = {
   apps: [
     {
       name: "ferdi.digital",
-      port: "3002",
       script: ".output/server/index.mjs",
+      exec_mode: "fork",
+      instances: 1,
       env: {
         NODE_ENV: "production",
         NITRO_PORT: 3002,
+        NITRO_HOST: "0.0.0.0",
       },
       env_file: ".env",
     },
